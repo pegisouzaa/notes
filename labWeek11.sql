27ff43d95915dd436cf67f1d5b4bd22c0b1ab1aa
Pedro <101530025@georgebrown.ca>
2024-11-14 10:57:14 -0500

    Add files via upload

---
+++ labWeek11.sql
@@ -0,0 +1,30 @@
+--Ex.1
+--SELECT title, due_date
+--FROM  assignments
+--WHERE course_id like 'COMP1234%';
+
+--Ex.2
+--SELECT min(due_date) FROM assignments;
+
+
+--Ex.3
+--SELECT max(due_date) FROM assignments;
+
+--Ex.4
+--SELECT title, course_id
+--FROM assignments
+--WHERE due_date like '2024-10-08';
+
+--Ex.5
+--SELECT title, due_date
+--from assignments
+--WHERE due_date LIKE '2024-10%';
+
+--Ex.6
+--SELECT course_id, title, status, due_date
+--FROM assignments
+--WHERE status = 'Completed'
+--ORDER BY due_date;
+
+
+
